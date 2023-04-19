c6b00086d7a85bea05550c7e8d2bad6247f19499
Manasa <manasa.g.pavushetty@gmail.com>
2023-04-19 17:09:28 +0530

    second commit

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import homepage,Brandpages,Categorypage
+from .views import homepage,Brandpages,Categorypage,login,signup,product_view
 urlpatterns = [ 
     path('',homepage,name='home'),
     path('brand/<slug:nm>',Brandpages,name='brand'),
-    path('category/<slug:nm>',Categorypage,name='category')
+    path('category/<slug:nm>',Categorypage,name='category'),
+    path('register/',signup,name='register'),
+    path('login/',login,name='login'),
+    path('logout/',login,name='logout'),
+    path('product/<str:nm>',product_view,name='product'),
     ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
-from .models import Brand,Category,Product
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from urllib.parse import unquote
+ #to remove %20 in the product name 
 
+from django.shortcuts import render
+from django.contrib.auth.models import User
+
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Brand,Category,Product
+from django.contrib.auth import authenticate,login as auth_login,logout
 
 def navbar_items(request):
       brand_items = Brand.objects.all()
@@ -10,7 +18,38 @@ def navbar_items(request):
                         }
       return context
      
+def signup(request):
+    if request.method == 'POST':
+        print('hello')
+        username =  request.POST['username']
+        email =  request.POST['email']
+        password =  request.POST['password']
+        c_password = request.POST['confirm_password']
+        if (password == c_password):
+            print('sucess')
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+            messages.info(request,"you have created a new account")
+            return redirect(login)
+        else:
+           messages.info(request,"password does not match")
+    return render(request, 'signup.html')
 
+def login(request):
+    if request.method == 'POST':
+            username =  request.POST['username']
+            password =  request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                auth_login(request,user)
+                print("logged in user")
+                messages.info(request,"you have logged in succesfully")
+                return redirect(homepage)
+            else:
+                messages.info(request,"username or password incorrect")
+    return render(request, 'login.html')
+
+@login_required(login_url="/login/")
 def homepage(request):
     brand = Brand.objects.all()
     category = Category.objects.all()
@@ -21,6 +60,7 @@ def homepage(request):
     context.update(navbar_items(request))
     return render(request, 'homepage.html',context)
 
+@login_required(login_url="/login/")
 def Brandpages(request,nm):
     brand = Brand.objects.get(name=nm)
     products = Product.objects.filter(brand=brand)
@@ -29,6 +69,7 @@ def Brandpages(request,nm):
     return render(request, 'brand.html',context)
 
 
+@login_required(login_url="/login/")
 def Categorypage(request,nm):
     category = Category.objects.get(name=nm)
     products = Product.objects.filter(Category=category)
@@ -36,3 +77,14 @@ def Categorypage(request,nm):
     context.update(navbar_items(request))
     return render(request, 'category.html',context)
 
+@login_required(login_url="/login/")
+def logout_view(request):
+    logout(request)
+    return redirect(login)
+
+def product_view(request,nm):
+    product= get_object_or_404(Product,name=nm)
+    print(product.id)
+    context = {'product':product}
+    context.update(navbar_items(request))
+    return render(request, 'product.html',context)
